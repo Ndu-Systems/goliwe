98988fba51cf905b36e5c9a3411f81a8944991c8
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-05-03 06:41:11 +0200

    Template CSS and Index and Folder structures

---
+++ scripts/core/angular/routes.js
@@ -0,0 +1,15 @@
+﻿app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: 'components/home/home.html',
+            controller: 'HomeCtrl'
+        })
+        .when("/Home", {
+            templateUrl: 'components/home/home.html',
+            controller:'HomeCtrl'
+        })
+        .when("/User-Login", {
+            templateUrl: 'components/login/login.html',
+            controller: 'LoginCtrl'
+        })       
+}); 
