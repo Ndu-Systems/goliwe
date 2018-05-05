526d7e5c076dc3f201f91b0548c6e128e64fdc74
NdumisoMthembu <mrnnmthembu@gmail.com>
2018-05-05 20:32:10 +0200

    Clecl for 0 articles

--- components/home/HomeCtrl.js
+++ components/home/HomeCtrl.js
@@ -1,1 +1,14 @@
-app.controller("HomeCtrl", function ($http, $scope, $window, $timeout, $location) { })
+app.controller("HomeCtrl", function ($http, $scope, $window, GetArticlesService, $location) {
+    GetArticlesService.GetArticles(
+        function (response) {
+            $scope.articles = response.data;
+            if ($scope.articles.length) {
+                $scope.message = 'No articles found!'
+            }
+        }
+    );
+    $scope.More = function (item) {
+        alert(item.Price);
+    }
+
+})
