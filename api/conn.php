6daf0e1639acef8504ec9466cdc29ab82df8ae66
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-05-14 20:01:26 +0200

    Migration to Angullar 5

--- api/conn.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = new PDO('mysql:host=127.0.0.1;dbname=ndusys0_guliwe','ndusys0_admin','Harder01!');
-//$conn = new PDO('mysql:host=localhost;dbname=guliwedb','root','');
-?>
-
--- components/logout/LogoutCtrl.js
+++
@@ -1,6 +0,0 @@
-app.controller("LogoutCtrl", function ($http, $scope, $window, $timeout, $location) {
-
-    localStorage.clear();
-    $location.path('/');
-
-})
